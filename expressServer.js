084e35343a519d4b93fb4148034dd04ac4eccbf6
tpeterec <alex1996p3@gmail.com>
2023-04-04 14:51:53 -0800

    Part 1 RESTful done

--- expressServer.js
+++ expressServer.js
@@ -1,30 +1,33 @@
 // set up dependencies
+require("dotenv").config();
 const fs = require("fs");
 const express = require("express");
 const app = express();
-const port = process.env.port || 3000;
-
+const port = process.env.PORT || 3000;
+app.use(express.json());
 // handles requests w/ routes
 app.get("/pets", (req, res) => {
   fs.readFile("pets.json", "utf-8", (error, pets) => {
     if (error) {
-      res.error(error);
+      res.err(error);
     } else {
-      res.json(JSON.parse(pets));
-      res.status(200);
-      res.end();
+      // res.json(JSON.parse(pets));
+      res.status(200).send(pets);
+      // res.status(200);
+      // res.end();
     }
   });
 });
+
 app.get("/pets/:petId", (req, res) => {
   fs.readFile("pets.json", "utf-8", (error, pets) => {
     if (error) {
-      res.error(error);
+      res.err(error);
     }
     let allPets = JSON.parse(pets);
     let index = req.params.petId;
     if (index < 0 || index > allPets.length || isNaN(index)) {
-      res.writeHead(404,({"content-type": "text/plain"}));
+      res.writeHead(404, { "content-type": "text/plain" });
       res.write("Not Found");
       res.end();
     }
---
+++ restfulExpressServer.js
@@ -0,0 +1,83 @@
+
+// set up dependencies
+require("dotenv").config();
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
+
+
+// handles requests w/ routes
+app.get("/pets", (req, res) => {
+    fs.readFile("pets.json", "utf-8", (error, pets) => {
+        if (error) {
+            res.err(error);
+        } else {
+            res.status(200).send(pets);
+            
+        }
+    });
+});
+
+
+app.get("/pets/:petId", (req, res) => {
+    fs.readFile("pets.json", "utf-8", (error, pets) => {
+        if (error) {
+            res.err(error);
+        }
+        let allPets = JSON.parse(pets);
+        let index = req.params.petId;
+        if (index < 0 || index > allPets.length || isNaN(index)) {
+            res.status(404).send("Not Found");
+        }
+        let singlePet = allPets[index];
+        res.send(singlePet);
+    });
+});
+// Post request creating a pet
+app.post('/pets', (req, res) => {
+    fs.readFile("pets.json", "utf-8", (error, data) => {
+        let pets = JSON.parse(data);
+        let age = req.body.age;
+        let kind = req.body.kind;
+        let name = req.body.name;
+        let newObj = { age: age, kind: kind, name: name };
+        pets.push(newObj);
+        fs.writeFile("pets.json", JSON.stringify(pets), () => {
+          console.log(newObj);
+          res.send(newObj);
+        });
+      });
+});
+
+app.patch("/pets/:petId", (req, res) => {
+    fs.readFile("pets.json", "utf-8", (error, data) => {
+        let pets = JSON.parse(data);
+        let key = Object.keys(req.body)[0];
+        let value = Object.values(req.body)[0];
+        let index = req.params.petId;
+        pets[index][key] = value;
+        fs.writeFile("pets.json", JSON.stringify(pets), () => {
+          //console.log(newObj);
+          res.send(pets[index]);
+        });
+      });
+});
+
+app.delete("/pets/:petId", (req, res) => {
+    fs.readFile("pets.json", "utf-8", (error, data) => {
+        let pets = JSON.parse(data);
+        let index = req.params.petId;
+        console.log(pets[index]);
+        let response = pets[index];
+        pets.splice(index, 1);
+        fs.writeFile("pets.json", JSON.stringify(pets), () => {});
+        res.send(response);
+      });
+});
+
+//listen on a port
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
